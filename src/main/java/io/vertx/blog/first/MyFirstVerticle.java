6ab4c79b639bd32419928a76ffe84f470b2e52dc
m00k <christian.bican@gmail.com>
2016-07-19 08:12:07 +0200

    feat: index.html

--- src/main/java/io/vertx/blog/first/MyFirstVerticle.java
+++ src/main/java/io/vertx/blog/first/MyFirstVerticle.java
@@ -2,8 +2,10 @@ package io.vertx.blog.first;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 /**
  * Created by m00k on 18.07.16.
@@ -13,15 +15,7 @@ public class MyFirstVerticle extends AbstractVerticle {
     @Override
     public void start(Future<Void> fut) throws Exception {
         int port = config().getInteger("http.port", 8080);
-        Router router = Router.router(vertx);
-
-        router.route("/").handler(routingContext -> {
-            HttpServerResponse response = routingContext.response();
-            response
-                .putHeader("content-type", "text/html")
-                .end("<h1>vert.x first</h1>")
-            ;
-        });
+        Router router = initRouter(vertx);
 
         vertx
             .createHttpServer()
@@ -36,5 +30,25 @@ public class MyFirstVerticle extends AbstractVerticle {
                     }
                 }
             );
+    }
+
+    private Router initRouter(Vertx vertx) {
+        Router router = Router.router(vertx);
+
+        router
+            .route("/")
+            .handler(routingContext -> {
+                HttpServerResponse response = routingContext.response();
+                response
+                    .putHeader("content-type", "text/html")
+                    .end("<h1>vert.x first</h1>")
+                ;
+            });
+
+        router
+            .route("/assets/*")
+            .handler(StaticHandler.create("assets"));
+
+        return router;
     }
 }
