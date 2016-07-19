b44d18e75763304d1298921b96943416575e4b0d
m00k <christian.bican@gmail.com>
2016-07-19 07:49:46 +0200

    feat: use vertx-web router for root page

--- src/main/java/io/vertx/blog/first/MyFirstVerticle.java
+++ src/main/java/io/vertx/blog/first/MyFirstVerticle.java
@@ -2,6 +2,8 @@ package io.vertx.blog.first;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 /**
  * Created by m00k on 18.07.16.
@@ -11,11 +13,19 @@ public class MyFirstVerticle extends AbstractVerticle {
     @Override
     public void start(Future<Void> fut) throws Exception {
         int port = config().getInteger("http.port", 8080);
+        Router router = Router.router(vertx);
+
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response
+                .putHeader("content-type", "text/html")
+                .end("<h1>vert.x first</h1>")
+            ;
+        });
+
         vertx
             .createHttpServer()
-            .requestHandler(r ->
-                r.response().end("<h1>vert.x first</h1>")
-            )
+            .requestHandler(router::accept)
             .listen(
                 port,
                 result -> {
